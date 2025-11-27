4b126d27f2832811a60c9a534647fc605c683bd9
Sijadkhan1678 <Sijadkhan1678@gmail.com>
2025-11-27 19:34:31 +0500

    feat(product): add additional fields to db schema

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,26 +3,82 @@ const { Schema } = mongoose;
 
 const productSchema = new Schema(
   {
-    name: { type: String, required: true },
+    name: {
+      type: String,
+      required: true
+    },
     slug: {
+      type: String
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    stock: {
+      type: Number,
+      default: 0
+    },
+    images: [{ type: String, required: true }],
+    bottomStyle: {
       type: String,
     },
-    price: { type: Number, required: true },
-    description: { type: String, trim: true },
-    stock: { type: Number, default: 0 },
-    images: [{ type: String }],
-
+    colorType: {
+      type: String,
+      required: true
+    },
+    dupattaType: {
+      type: String,
+    },
+    productId: {
+      type: String,
+    },
+    liningAttached: {
+      type: String,   
+    },
+    noOfPieces: {
+      type: String,
+      required: true
+    },
+    season: {
+      type: String,
+      required: true
+    },
+    shirtFabric: {
+      type: String,
+      required: true
+    },
+    topStyle: {
+      type: String,
+      required: true
+    },
+    trouserStyle: {
+      type: String,
+    },
+    additionalDescription: {
+      type: String,
+      required: true
+    },
+    disclaimer: {
+      type: String,
+      required: true
+    },
+    workTechnique: {
+      type: String
+    },
     mainCategory: {
       type: Schema.Types.ObjectId,
       ref: "Category",
-      required: true,
+      required: true
     },
-
     subCategory: {
       type: String,
       required: true,
-      trim: true,
-    },
+      trim: true
+    }
   },
   { timestamps: true }
 );
