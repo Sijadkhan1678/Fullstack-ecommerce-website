444fd16f8a67a7d536260697bc8e60b9be95c5da
Sijadkhan1678 <Sijadkhan1678@gmail.com>
2026-01-26 09:39:56 +0500

    add further routes for auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
-const { getUser, register, login } = require('../controllers/auth.controller');
+const { getUser, register, login, updateAvatar,deleteUser } = require('../controllers/auth.controller');
 const config = require('../config')
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = config.get('JWT_SECRET')
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/profile')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix + "." + file.mimetype.split('/')[1])
+    }
+  })
+  const upload = storage
 
+router.route('/register').post([
+    body('role', 'please provide role').notEmpty().trim(),
+    body('fullName', 'please provide fullName', "fullname at least 4 charactors").isLength({ min: 4 }).trim(),
+    body('username').trim(),
+    body('email', 'please provide a valid email').isEmail().trim(),
+    body('password', 'Please enter password at least 8 charactors').isLength({ min: 8 }).trim(),
+], register)
+
+router.route('/login').post([
+    body('email', 'please provide a valid email').isEmail().trim(),
+    body('password', 'Please enter password at least 8 charactors').isLength({ min: 8 }).trim()
+], login)
+
+router.route('/user').get(verifyAuthToken, getUser)
+
+router.delete("/user:id",verifyAuthToken,deleteUser)
+// router.route('/avatar').put(upload.single("avatar"),updateAvatar)
+// router.route('/user/updatePassword').put([verifyAuthToken,updatePassword)
+// router.route('/user/addusername').put([body("username","Username at least 4 chararctors long").isLength({min:4}).trim(),verifyAuthToken],updateUsername)
+// router.route('/user/email').put(verifyAuthToken,updateEmail)
+// router.route('/user/delete').delete(verifyAuthToken,deleteUser)
+// console.dir(router.stack[1].route)
+module.exports = router
+
+// middlewares
 function verifyAuthToken(req, res, next) {
     try {
 
@@ -22,21 +58,6 @@ function verifyAuthToken(req, res, next) {
     }
 
 }
-router.route('/user').get(verifyAuthToken, getUser)
-
-router.route('/register').post([
-    body('role', 'please provide role').notEmpty().trim(),
-    body('fullName', 'please provide fullName', "fullname at least 4 charactors").isLength({ min: 4 }).trim(),
-    body('username').trim(),
-    body('email', 'please provide a valid email').isEmail().trim(),
-    body('password', 'Please enter password at least 8 charactors').isLength({ min: 8 }).trim(),
-], register)
-
-router.route('/login').post([
-    body('email', 'please provide a valid email').isEmail().trim(),
-    body('password', 'Please enter password at least 8 charactors').isLength({ min: 8 }).trim()
-], login)
 
 
-// console.log('router',router)
-module.exports = router
+  
