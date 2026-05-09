bdcc85d49df6567db933282f3981d64248bfd155
Sijadkhan1678 <Sijadkhan1678@gmail.com>
2026-05-09 10:21:33 +0500

    Implemented create category logic

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -4,9 +4,39 @@ const Category = require("../models/Catagory")
 exports.createCategory = async (req, res) => {
     try {
         const { name, slug, image, parentId, description, isActive } = req.body
-        res.status(201).json({ message: "create category" })
+        
+        // db call for category whether category exist or not on basis of name and slug or parentId  
+
+        const existCategory = await Category.findOne({ $or: [{ name }, { _id: parentId }] }).select("_id name slug ancestors");
+        // console.log(existCategory?._id)
+        const category = new Category({ name, slug, image, description, isActive, })
+        // console.log(category)
+        if (existCategory.name === name) {
+
+            return res.status(409).json({ success: false, message: 'category already exist with this name' })
+
+        }
+        if(existCategory.ancestors.find((category)=> name === name)){
+         return res.status(409).json({success: false, message: "this"})
+        }
+        // check for parent category existence in the database 
+        if (existCategory?._id.equals(parentId)) {
+            console.log("parentId === existcategoryid")
+            const { _id, name, slug, ancestors, } = existCategory
+            const parentCategory = {
+                _id,
+                name,
+                slug
+            }
+            ancestors.length ? category.ancestors.push(...ancestors, parentCategory) : category.ancestors.push(parentCategory)
+            category.slug = slug + '-' + category.slug
+            category.parentId = parentId
+        }
+
+        await category.save()
+        res.status(201).json({ success: true, data: { category } })
     } catch (err) {
-        rest.status(500).json({ success: false, message: "Server Error", error: err.message })
+        res.status(500).json({ success: false, message: "Server Error", error: err.message })
     }
 
 }
@@ -15,7 +45,7 @@ exports.getCategories = async (req, res) => {
     try {
         return res.status(404).json({ message: "categories" })
     } catch (err) {
-        rest.status(500).json({ success: false, message: "Server Error", error: err.message })
+        res.status(500).json({ success: false, message: "Server Error", error: err.message })
     }
 }
 exports.getCategory = async (req, res) => {
@@ -23,7 +53,7 @@ exports.getCategory = async (req, res) => {
     try {
         res.status(200).json({ messge: "get single category" })
     } catch (err) {
-        rest.status(500).json({ success: false, message: "Server Error", error: err.message })
+        res.status(500).json({ success: false, message: "Server Error", error: err.message })
     }
 
 }
@@ -35,6 +65,6 @@ exports.deleteCategory = async (req, res) => {
         res.status(200).json({ success: true, message: "Category successfully deleted" })
 
     } catch (err) {
-        rest.status(500).json({ success: false, message: "Server Error", error: err.message })
+        res.status(500).json({ success: false, message: "Server Error", error: err.message })
     }
 }
