bcc6711a3921c0501532ea716b97bd5f88890552
Sijadkhan1678 <Sijadkhan1678@gmail.com>
2026-05-15 10:26:10 +0500

    add categoryImageUPloader to category routes

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const { categoryValidationRules, validate } = require("../middlewares/categoryValidator")
+const categoryImageUPloader = require("../middlewares/uploader")
 const { createCategory, getCategories, getCategory, deleteCategory } = require("../controllers/categories.controller")
 
-router.post('/', [categoryValidationRules(), validate], createCategory)
+router.post('/', [/*categoryImageUPloader*/,categoryValidationRules(), validate], createCategory)
 router.get('/', getCategories)
 router.get('/:id', getCategory)
 router.delete('/:id', deleteCategory)
